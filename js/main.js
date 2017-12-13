b8fcbc9ec01183b48aef9b4a409b4bbcf3e0027d
goebers <kack@cock.li>
2017-12-14 00:52:20 +0200

    lol hups

--- js/main.js
+++ js/main.js
@@ -10,18 +10,26 @@ $(document).ready(function () {
   button.addEventListener('click', (e) => {
 
     let url = input.value;
-    let tmp = '';
+    let tmp;
+    let videoId;
 
     if (url.includes('&list=')) { // full url
       url = url.substring(0, url.indexOf('&list='));
       tmp = url.split('watch?v=');
-
+      videoId = tmp[tmp.length - 1];
     } else if (url.includes('?list=')) { // shortened url
       url = url.substring(0, url.indexOf('?list='));
       tmp = url.split('youtu.be/');
+      videoId = tmp[tmp.length - 1];
     }
 
-    let videoId = tmp[tmp.length - 1];
+    if (url.includes('youtu.be/')) {
+      tmp = url.split('youtu.be/');
+      videoId = tmp[tmp.length - 1];
+    } else if (url.includes('watch?v=')) {
+      tmp = url.split('watch?v=');
+      videoId = tmp[tmp.length - 1];
+    }
 
     let final = 'https://i.ytimg.com/vi/' + videoId + '/hqdefault.jpg'
 
